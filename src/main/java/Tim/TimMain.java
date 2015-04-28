2945fea544b2796955ed7f6e3129169046e04890
jjkramok@gmail.com <Appelsap7>
2015-04-28 15:31:36 +0200

    Initial commit

---
+++ src/main/java/Tim/TimMain.java
@@ -0,0 +1,29 @@
+package Tim;
+
+import db.ConnectDatabase;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by tim on 28-4-15.
+ */
+public class TimMain {
+    static String[] str = null;
+    public static Connection connection = ConnectDatabase.getInstance().getConnection();
+
+    public static void main(String[] args) {
+        try {
+            Opgave6.main(str);
+
+
+
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
