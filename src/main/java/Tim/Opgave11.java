8ab68564e716d89caa568be89af019a0c1776097
jjkramok@gmail.com <Appelsap7>
2015-04-28 16:45:06 +0200

    Finished opgave11

--- src/main/java/Tim/Opgave11.java
+++ src/main/java/Tim/Opgave11.java
@@ -1,25 +1,62 @@
 package Tim;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by tim on 28-4-15.
  */
 public class Opgave11 implements Opgave {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        ResultSet statementSet = null;
+        ResultSet prepStatementSet = null;
+        ResultSet procedureSet = null;
+
+
         int iters=100;
         long startTime = System.currentTimeMillis();
         // voorbereidende statements buiten de loop
+        Statement statement = TimMain.connection.createStatement();
+        PreparedStatement prepStatement = TimMain.connection.prepareStatement(getPreparedStatement());
+        Statement procedure = TimMain.connection.createStatement();
+
         // bijvoorbeeld Statement st = conn.createStatement();
         for (int i=0; i<iters; i++) {
             // statements die je 100x wilt doen
+
+            //Normal Statement
+            statementSet = statement.executeQuery(getStatement());
+            //Prepared Statement
+            prepStatementSet = prepStatement.executeQuery();
+            //Procedure
+            procedureSet = procedure.executeQuery(getProcedure());
         }
         // afrondende statements buiten de loop (bijv “st.close();”)
+
+        System.out.println("Statement: \n");
+        while (statementSet.next()) {
+            System.out.println(statementSet.getString(1));
+        }
+
+        System.out.println("Prepared Statement: \n");
+        while (prepStatementSet.next()) {
+            System.out.println(prepStatementSet.getString(1));
+        }
+
+        System.out.println("Procedure: \n");
+        while (procedureSet.next()) {
+            System.out.println(procedureSet.getString(1));
+        }
+
         long stopTime = System.currentTimeMillis();
         double elapsedTime = (stopTime - startTime) / (1.0*iters);
         System.out.println("Gemeten tijd: "+elapsedTime+" ms");
     }
 
-    public static String getQuery() {
-        return
+    public static String getStatement() throws SQLException {
+        String query =
                 "SELECT haut.name FROM Person haut, Writes hw\n" +
                 "WHERE haut.pid = hw.pid\n" +
                 "AND NOT EXISTS (SELECT aut.name FROM Person aut, Movie m, Writes w\n" +
@@ -28,6 +65,23 @@ public class Opgave11 implements Opgave {
                 "AND w.mid = m.mid\n" +
                 "AND (EXISTS (SELECT d FROM Directs d\n" +
                         "WHERE m.mid = d.mid)));";
+        return query;
+    }
 
+    public static String getPreparedStatement() throws SQLException {
+        String query =
+                "SELECT haut.name FROM Person haut, Writes hw\n" +
+                        "WHERE haut.pid = hw.pid\n" +
+                        "AND NOT EXISTS (SELECT aut.name FROM Person aut, Movie m, Writes w\n" +
+                        "WHERE aut.pid = w.pid\n"  +
+                        "AND aut.name = haut.name\n" +
+                        "AND w.mid = m.mid\n" +
+                        "AND (EXISTS (SELECT d FROM Directs d\n" +
+                        "WHERE m.mid = d.mid)));";
+        return query;
+    }
+
+    public static String getProcedure() throws SQLException {
+        return "SELECT opgave11gerben() \t";
     }
 }
